a567d0c772ada6d44abf5671c896856c095f3b13
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-07-07 14:17:01 +0100

    add: snapshot testing for Key

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Key.css';
 
-const Key = ({ keyAction, keyType, keyValue }) => <div className="key-container" />;
-
-Key.propTypes = {
-  keyAction: PropTypes.func.isRequired,
-  keyType: PropTypes.string.isRequired,
-  keyValue: PropTypes.string.isRequired,
-}
+const Key = ({ keyAction, keyType, keyValue }) => (
+  <div className={`key-container ${keyType}`}>
+    <p className="key-value">
+      {keyValue}
+    </p>
+  </div>
+);
 
 export default Key;

e5668230bb5ddcaa8e9f80003de616e09c0ac60a
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-07-07 14:37:48 +0100

    add: snapshot test for Keypad

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -18,6 +18,8 @@ describe('Keypad', () => {
     );
   });
 
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render 4 <div />\'s', () => {
     expect(wrapper.find('div').length).toEqual(4);
   });
